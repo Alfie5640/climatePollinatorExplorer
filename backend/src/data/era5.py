a1025f1f191f250862773318624d08a6e99e4c52
Alfie5640 <alfiewalker5640@gmail.com>
2026-07-15 19:24:53 +0100

    feature eng, started refactor

---
+++ backend/src/data/era5.py
@@ -0,0 +1,21 @@
+
+import earthkit.data as ekd
+
+def load_era5(year, month, days, area):
+    temp_data = ekd.from_source(
+        "cds",
+        "reanalysis-era5-single-levels",
+        variable="2m_temperature",
+        product_type="reanalysis",
+        year=str(year),
+        month=str(month).zfill(2),
+        day=days,
+        time=["12:00"],
+        area=area,
+        format="netcdf",
+    )
+
+    ds = temp_data.to_xarray()
+    ds["t2m_C"] = ds["t2m"] - 273.15
+
+    return ds
